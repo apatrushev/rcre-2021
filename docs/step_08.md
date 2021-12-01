236f20bb37f39625fa83027825c8f0682a14815e
Anton Patrushev <apatrushev@gmail.com>
2021-12-02 03:38:26 +0500

    rtic, step_08

---
+++ docs/step_08.md
@@ -0,0 +1,7 @@
+```
+$ cargo add panic-halt cortex-m-rtic
+    Updating 'https://github.com/rust-lang/crates.io-index' index
+      Adding panic-halt v0.2.0 to dependencies
+      Adding cortex-m-rtic v0.5.9 to dependencies
+```
+этого примера уже нет в BSP. не беда - напишем сами! смотрим код!
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,55 @@
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_std]
 #![no_main]
 
-use core::{
-    panic::PanicInfo,
-    sync::atomic::{AtomicBool, Ordering},
-};
-use stm32f3 as _;
+extern crate rtic;
+
+use panic_halt as _;
 use stm32f3_discovery::{
     button,
     button::interrupt::TriggerMode,
     leds::Leds,
-    stm32f3xx_hal::{interrupt, pac, prelude::*},
+    stm32f3xx_hal::{prelude::*},
     switch_hal::ToggleableOutputSwitch,
-    wait_for_interrupt,
 };
 
-static USER_BUTTON_PRESSED: AtomicBool = AtomicBool::new(false);
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[interrupt]
-fn EXTI0() {
-    button::interrupt::clear();
-    USER_BUTTON_PRESSED.store(true, Ordering::Relaxed);
-}
-
-#[cortex_m_rt::entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-
-    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    let mut leds = Leds::new(
-        gpioe.pe8,
-        gpioe.pe9,
-        gpioe.pe10,
-        gpioe.pe11,
-        gpioe.pe12,
-        gpioe.pe13,
-        gpioe.pe14,
-        gpioe.pe15,
-        &mut gpioe.moder,
-        &mut gpioe.otyper,
-    );
-
-    button::interrupt::enable(&dp.EXTI, &dp.SYSCFG, TriggerMode::Rising);
-
-    loop {
-        if USER_BUTTON_PRESSED.swap(false, Ordering::AcqRel) {
-            leds.ld3.toggle().ok();
-        }
-
-        wait_for_interrupt();
+#[rtic::app(device = stm32f3_discovery::stm32f3xx_hal::pac, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        leds: Leds,
     }
-}
+
+    #[init]
+    fn init(ctx: init::Context) -> init::LateResources {
+        let mut rcc = ctx.device.RCC.constrain();
+        let mut gpioe = ctx.device.GPIOE.split(&mut rcc.ahb);
+
+        let leds = Leds::new(
+            gpioe.pe8,
+            gpioe.pe9,
+            gpioe.pe10,
+            gpioe.pe11,
+            gpioe.pe12,
+            gpioe.pe13,
+            gpioe.pe14,
+            gpioe.pe15,
+            &mut gpioe.moder,
+            &mut gpioe.otyper,
+        );
+
+        button::interrupt::enable(
+            &ctx.device.EXTI,
+            &ctx.device.SYSCFG,
+            TriggerMode::Rising,
+        );
+
+        init::LateResources { leds }
+    }
+
+    #[task(binds = EXTI0, resources = [leds])]
+    fn button_click(ctx: button_click::Context) {
+        button::interrupt::clear();
+        ctx.resources.leds.ld3.toggle().ok();
+    }
+};
